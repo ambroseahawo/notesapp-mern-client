b3020215f8ae6b890e0dd0b57ca03e1b783a6d87
Ambrose Ahawo <ahawoadey995@gmail.com>
2023-01-26 05:39:44 +0300

    Refactor Api Slice

--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -11,7 +11,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError
       },
-      keepUnusedDataFor: 5,  // default 60seconds
+      // keepUnusedDataFor: 5,  // default 60seconds
       transformResponse: responseData => {
         const loadedUsers = responseData.map(user => {
           user.id = user._id
