c2016391ab033db3234b8c42fe7517c175fc6969
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-01-30 05:03:30 +0300

    Refactor Welcome Page

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -12,10 +12,16 @@ const Welcome = () => {
       <p>{today}</p>
       <h1>Welcome!</h1>
       <p>
-        <Link to="/dash/notes">View TechNotes</Link>
+        <Link to="/dash/notes">View techNotes</Link>
+      </p>
+      <p>
+        <Link to="/dash/notes/new">Add New techNote</Link>
       </p>
       <p>
         <Link to="/dash/users">View User Settings</Link>
+      </p>
+      <p>
+        <Link to="/dash/users">Add New User</Link>
       </p>
     </section>
   )
